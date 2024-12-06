919e80d90f502cb6a580ef30e021d56b72dbc280
Parsa Farshadfar <46465419+parsafarshadfar@users.noreply.github.com>
2024-12-05 23:05:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def test_proxy(proxy, speed_threshold=2):
 
 def get_pytrends_instance_with_retries(keywords, timeframe, is_region=False):
     # Try without proxy first
-    pytrends = TrendReq(hl='en-US', tz=360, requests_args={'timeout': 10})
+    pytrends = TrendReq(hl='en-US', tz=360, requests_args={'timeout': 5})
     try:
         pytrends.build_payload(keywords, timeframe=timeframe)
         if is_region:
@@ -95,7 +95,7 @@ def get_pytrends_instance_with_retries(keywords, timeframe, is_region=False):
                             hl='en-US', 
                             tz=360, 
                             proxies=proxy, 
-                            requests_args={'timeout': 10}
+                            requests_args={'timeout': 5}
                         )
                         pytrends.build_payload(keywords, timeframe=timeframe)
                         if is_region:
